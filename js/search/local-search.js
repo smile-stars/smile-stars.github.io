5acdc9dca4b45a079a6bd9263e1dc1f7bc9ff4d8
songyanyan <15890152837@163.com>
2023-12-14 14:44:13 +0800

    Site updated: 2023-12-14 14:44:12

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -190,7 +190,7 @@ window.addEventListener("load", () => {
 
               str += '<div class="local-search__hit-item">';
               if (oneImage) {
-                str += `<div class="search-left"><img src=${oneImage} alt=${dataTitle} data-fancybox='gallery'>`;
+                str += `<div class="search-left"><img src=${oneImage} alt=${dataTitle} data-fancybox='gallery' referrerpolicy='no-referrer'>`;
               } else {
                 str += '<div class="search-left" style="width:0">';
               }
